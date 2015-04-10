e16587e3e4451733164f720ad4a6b8eab4be74bf
Prashanth Subrahmanyam <psubrahmanyam@apigee.com>
2015-04-10 16:07:48 +0530

    added finder to iterm

--- setup-antigen.sh
+++ setup-antigen.sh
@@ -12,6 +12,10 @@ if ! [ -f "$HOME/.laptop.antidone" ]; then
   append_to_file "$HOME/.zshrc" "antigen bundle heroku"
   append_to_file "$HOME/.zshrc" "antigen bundle command-not-found"
   append_to_file "$HOME/.zshrc" "antigen bundle zsh-users/zsh-syntax-highlighting"
+  append_to_file "$HOME/.zshrc" "antigen bundle robbyrussell/oh-my-zsh plugins/ruby"
+  append_to_file "$HOME/.zshrc" "antigen bundle zsh-users/zsh-syntax-highlighting"
+  append_to_file "$HOME/.zshrc" "antigen bundle zsh-users/zsh-syntax-highlighting"
+  append_to_file "$HOME/.zshrc" "antigen bundle zsh-users/zsh-syntax-highlighting"
 
   # Load the theme.
   append_to_file "$HOME/.zshrc" "antigen theme agnoster"
